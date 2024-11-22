7488060a2d42e6531327df56349320fa55811bfd
jb2116 <juliobatista2116@gmail.com>
2024-11-22 18:27:12 -0300

    Adicionando estilização ao header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,19 +7,21 @@ const Header = () => {
             <div className={styles.header_header}>
                 <img src="src/assets/logo.svg" alt="" />
                 <div className={styles.header_search_bar}>
-                    <span>Pesquisar produto...</span>
+                    <input className={styles.header_search} placeholder='Pesquisar produto...'></input>
                     <img className={styles.search_icon} src="src/assets/Search.svg" alt="" />
                 </div>
                 <div className={styles.header_login}>Cadastre-se</div>
                 <button className={styles.header_button}>Entrar</button>
                 <img className={styles.header_carrinho} src="src/assets/carrinho.svg" alt="" />
             </div>
-            <ul className={styles.header_footer}>
-                <li>Home</li>
-                <li>Produtos</li>
-                <li>Categorias</li>
-                <li>Meus Pedidos</li>
-            </ul>
+            <div>
+                <ul className={styles.header_footer}>
+                    <li>Home</li>
+                    <li>Produtos</li>
+                    <li>Categorias</li>
+                    <li>Meus Pedidos</li>
+                </ul>    
+            </div>
             </div>
         </>
     )
