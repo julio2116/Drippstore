368f91c44b5afdc373a4eec07552da234c801397
unknown <juliosilva0558@digitalcollege.internal>
2024-11-29 11:41:12 -0300

    Ajustes antes de refatorar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                     <input className={styles.header_search} placeholder='Pesquisar produto...'></input>
                     <img className={styles.search_icon} src="src/assets/Search.svg" alt="" />
                 </div>
-                <div className={styles.header_login}>Cadastre-se</div>
+                <span className={styles.header_login}>Cadastre-se</span>
                 <button className={styles.header_button}>Entrar</button>
                 <img className={styles.header_carrinho} src="src/assets/carrinho.svg" alt="" />
             </div>
